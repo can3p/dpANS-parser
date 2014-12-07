9a5ef1ab338158598227dddb6fecc1607eac99fa
Robert Strandh <robert.strandh@gmail.com>
2014-12-07 12:29:18 +0100

    Added elementary parsers for an ordinary word and for a Tex macro name.

--- parse.lisp
+++ parse.lisp
@@ -1,1 +1,13 @@
 (cl:in-package #:dpans-parser)
+
+(define-parser word-parser
+  (singleton #'identity
+	     (lambda (token)
+	       (and (typep token 'identifier)
+		    (not (eql (char (contents token) 0) #\\))))))
+
+(define-parser macro-parser
+  (singleton #'identity
+	     (lambda (token)
+	       (and (typep token 'identifier)
+		    (eql (char (contents token) 0) #\\)))))
