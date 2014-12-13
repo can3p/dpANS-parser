fd44200289680643f03f5f1a2b3903f02e50dab4
Robert Strandh <robert.strandh@gmail.com>
2014-12-13 07:41:20 +0100

    Removed MACRO-PARSER which is no longer relevant.

--- parse.lisp
+++ parse.lisp
@@ -3,12 +3,6 @@
 (define-parser word-parser
   (singleton #'identity #'identifierp))
 
-(define-parser macro-parser
-  (singleton #'identity
-	     (lambda (token)
-	       (and (typep token 'identifier)
-		    (eql (char (contents token) 0) #\\)))))
-
 (define-parser punctuation-parser
   (singleton #'identity
 	     (lambda (token)
