d843ff47b3095c1ece5dffcef9e27bdd033ff12e
Dmitry Petrov <dpetroff@gmail.com>
2016-11-20 01:08:51 +0100

    full command parser

--- tex-command-parsers.lisp
+++ tex-command-parsers.lisp
@@ -16,25 +16,53 @@
 
 ;; TEXT = ((WORD | OPERATOR) WHITESPACE*)
 
+(defun pass-args (&rest args) args)
+(defun flatten-args (&rest args)
+  (apply #'concatenate 'list args))
+
+
 (defclass <command> ()
     (
-     (is-new :initarg :is-new :initform nil :reader is-new)
+     (is-definition :initarg :is-definition :initform nil :reader is-definition)
+     (args :initarg :args :initform nil :reader args)
      (name :initarg :name :reader name)
      ))
 
 (defmethod print-object ((instance <command>) stream)
-  (format stream "<command ~s>~%" (name instance)))
+  (format stream "<command ~s>~%Command args:~a~%"
+          (name instance)
+          (args instance)
+          ))
 
 ;;; This parser succeeds for a backslash followed by a word W with
 ;;; nothing in between the two.  It returns W as the result of the
 ;;; parse.
 (define-parser tex-command-parser
-  (consecutive (lambda (backslash command-word)
-		 (declare (ignore backslash))
+  (consecutive (lambda (backslash command-word args)
+                 (declare (ignore backslash))
                  (make-instance '<command>
-                                :name (contents command-word)))
-	       'backslash-parser
-	       'word-parser))
+                                :name (contents command-word)
+                                :args args))
+               'backslash-parser
+               'word-parser
+               (repeat* 'pass-args
+                        'tex-command-argument-parser)))
+
+(define-parser tex-command-argument-parser
+  (consecutive (lambda (start contents stop)
+                 (declare (ignore start stop)) contents)
+               'start-argument-parser
+               'tex-command-argument-inner-parser
+               'end-argument-parser))
+
+(define-parser tex-command-argument-inner-parser
+  (repeat* 'flatten-args
+           (consecutive 'flatten-args
+                        (repeat+ 'pass-args
+                                 (alternative 'word-parser
+                                              (narrow (lambda (punctuation) (not (string= (contents punctuation) "}"))) 'punctuation-parser)))
+                        (repeat* 'pass-args
+                                 (alternative 'whitespace-parser 'single-newline-parser)))))
 
 (define-parser word-parser
   (singleton #'identity #'identifierp))
@@ -78,4 +106,16 @@
 (define-parser backslash-parser
   (narrow (lambda (punctuation) (string= (contents punctuation) "\\"))
 	  'punctuation-parser))
+
+(define-parser start-argument-parser
+  (narrow (lambda (punctuation) (string= (contents punctuation) "{"))
+          'punctuation-parser))
+
+(define-parser end-argument-parser
+  (narrow (lambda (punctuation) (string= (contents punctuation) "}"))
+          'punctuation-parser))
+
+(define-parser ratio-parser
+  (narrow (lambda (punctuation) (string= (contents punctuation) "%"))
+          'punctuation-parser))
 
