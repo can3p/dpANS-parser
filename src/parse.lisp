696180fd52bbe4055ac4c0cbf716b4dda9bca21e
Dmitry Petrov <dpetroff@gmail.com>
2016-12-06 22:59:26 +0100

    reorganize api to have a main function that returns a parsed document

--- src/parse.lisp
+++ src/parse.lisp
@@ -60,13 +60,22 @@
         (print-xml t (create-document-from-stream stream))
         "failed to parse stream")))
 
-(defun test-create-document-from-file (&optional file)
+(defun test-create-and-print-document-from-file (&optional (file "concept-arrays"))
+  (let ((document (test-create-document-from-file file)))
+    (if document
+        (print-xml t document)
+        "failed-to-parse-stream")))
+
+(defun test-create-document-from-file (&optional (file "concept-arrays"))
   (let ((fname (make-pathname :directory '(:relative "dpans")
-                              :name (or file "concept-arrays") :type "tex"))
+                              :name file :type "tex"))
         (asdf-location (asdf:system-source-file :dpans-parser)))
     (with-open-file (in (merge-pathnames fname asdf-location))
       (let ((tokens (tokenize-stream in)))
         (multiple-value-bind (successp stream) (file-parser tokens)
           (if successp
-              (print-xml t (create-document-from-stream stream))
-              "failed to parse stream"))))))
+              (create-document-from-stream stream)
+              nil))))))
+
+(defun parse-spec ()
+  (test-create-document-from-file "chap-15"))
