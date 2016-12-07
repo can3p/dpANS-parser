2dfa23b97551ddd083de235aa7d0eaa3994612eb
Dmitry Petrov <dpetroff@gmail.com>
2016-12-07 22:37:33 +0100

    export symbols required for printing

--- src/packages.lisp
+++ src/packages.lisp
@@ -1,4 +1,36 @@
 (cl:in-package #:common-lisp-user)
 
 (defpackage #:dpans-parser
-  (:use #:common-lisp))
+  (:use #:common-lisp)
+  (:export :parse-spec
+           :<element>
+           :parent
+           :document
+           :<metavar>
+           :name
+           :<link>
+           :<seevar>
+           :<seefuns>
+           :<funref>
+           :<varref>
+           :<typeref>
+           :<chapref>
+           :<term>
+           :term
+           :text
+           :<new-term>
+           :<block-element>
+           :children
+           :<displaythree>
+           :title
+           :<displaythree-func>
+           :<container-block-element>
+           :title
+           :<document>
+           :props
+           :sections
+           :<paragraph>
+           :<formula>
+           :<formula-sub>
+           :<formula-symbol>
+   ))
